392ad116918d9ee8c746a3f1f21ebde6c879ba81
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2025-01-27 22:25:42 -0300

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+
+function adicionarAmigo() {
+    let nome_amigo = document.getElementById('amigo').value;
+    console.log(nome_amigo);
+}
+
+function sortearAmigo(){
+
+}
+
+function mostrarAmigo(){
+
+}
